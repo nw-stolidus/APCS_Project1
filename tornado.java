92b8de82a932d4838ae9c68c7323e4e5a9ce3c28
EC2 Default User <ec2-user@ip-172-31-72-22.ec2.internal>
2023-05-17 19:06:37 +0000

    first step

---
+++ tornado.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.*;
+
+class tornado {
+    int balance = 500;
+    String name = "Windell Blowout";
+    ArrayList<String> crops = new ArrayList<String>();
+    
+    public static int getBal() {
+        return balance
+    }
+    
+    public static int addBal(int x) {
+        balance = balance + x;
+    }
+    
+    
+    
+    
+	public static void main(String args[]) {
+		
+		
+		
+	}
+}
