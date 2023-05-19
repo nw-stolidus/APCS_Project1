6ca29d60c37fdb9bca431602e0af8134500b535f
EC2 Default User <ec2-user@ip-172-31-72-22.ec2.internal>
2023-05-19 18:34:33 +0000

    attempt

---
+++ starter.java
@@ -0,0 +1,16 @@
+import pkg.*;
+import java.util.*;
+
+class starter {
+    public static void main(String args[]) {
+        TornadoFarm test = new TornadoFarm();
+        test.newCrop("Squallermelon");
+        while(true) {
+            
+        }
+		
+		
+	}
+    
+    
+}
